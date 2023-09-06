83888e8e94c14009903fccc45f736b80c500f331
Zokerzes <yozhik13@gmail.com>
2023-09-06 20:25:52 +0300

    DbSet

--- TechStore/Data/AppDbContext.cs
+++ TechStore/Data/AppDbContext.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TechStore.Models;
 
 namespace TechStore.Data
 {
     public class AppDbContext:DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Maker> Maker { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+
     }
 }
